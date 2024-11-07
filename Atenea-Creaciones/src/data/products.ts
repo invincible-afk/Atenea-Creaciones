ef7d2d2a6ccdc2bcfbf0b528d4fd0eb6700c3e51
Max Ramo <128625330+invincible-afk@users.noreply.github.com>
2024-11-07 20:32:55 -0300

    Update products.ts

--- Atenea-Creaciones/src/data/products.ts
+++ Atenea-Creaciones/src/data/products.ts
@@ -2,25 +2,25 @@ export const products = [
   {
     title: "Ponchos Artesanales",
     description: "Descubre nuestra colección de ponchos tejidos a mano, donde la tradición se encuentra con el estilo contemporáneo. Cada pieza es única y está elaborada con los mejores materiales para brindarte calidez y confort.",
-    imageUrl: "/Assets/images/poncho.jpg",
+    imageUrl: "../Assets/images/poncho.jpg",
     category: "Ponchos"
   },
   {
     title: "Muñecos a Crochet",
     description: "Adorables creaciones hechas con amor y dedicación. Nuestros muñecos son compañeros perfectos para los más pequeños y coleccionables únicos para todas las edades.",
-    imageUrl: "/src/Assets/images/naturaleza-muerta-peluches.jpg",
+    imageUrl: "../Assets/images/naturaleza-muerta-peluches.jpg",
     category: "Amigurumis"
   },
   {
     title: "Bolsos Tejidos",
     description: "Elegancia y funcionalidad se unen en nuestra línea de bolsos tejidos. Diseños únicos que complementarán tu estilo personal con un toque artesanal.",
-    imageUrl: "/Assets/images/bolso.jpg",
+    imageUrl: "../Assets/images/bolso.jpg",
     category: "Accesorios"
   },
   {
     title: "Gorros y Bufandas",
     description: "Mantente abrigado con estilo. Nuestra colección de gorros y bufandas combina comodidad y diseño, perfecta para cualquier ocasión.",
-    imageUrl: "/Assets/images/gorros.jpg",
+    imageUrl: "../Assets/images/gorros.jpg",
     category: "Accesorios"
   },
   {
