b0d8dcaf81d051f9c14732ab7a9473081ceb3c59
Max Ramo <128625330+invincible-afk@users.noreply.github.com>
2024-11-13 20:43:08 -0300

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -8,25 +8,25 @@ export const products = [
   {
     title: "Muñecos a Crochet",
     description: "Adorables creaciones hechas con amor y dedicación. Nuestros muñecos son compañeros perfectos para los más pequeños y coleccionables únicos para todas las edades.",
-    imageUrl: "../src/Assets/images/naturaleza-muerta-peluches.jpg",
+    imageUrl: "/Assets/images/naturaleza-muerta-peluches.jpg",
     category: "Amigurumis"
   },
   {
     title: "Bolsos Tejidos",
     description: "Elegancia y funcionalidad se unen en nuestra línea de bolsos tejidos. Diseños únicos que complementarán tu estilo personal con un toque artesanal.",
-    imageUrl: "../src/Assets/images/bolso.jpg",
+    imageUrl: "/Assets/images/bolso.jpg",
     category: "Accesorios"
   },
   {
     title: "Gorros y Bufandas",
     description: "Mantente abrigado con estilo. Nuestra colección de gorros y bufandas combina comodidad y diseño, perfecta para cualquier ocasión.",
-    imageUrl: "../src/Assets/images/bufanda.jpg",
+    imageUrl: "/Assets/images/bufanda.jpg",
     category: "Accesorios"
   },
   {
     title: "Mantas de Apego",
     description: "Esta manta de apego tejida es el compañero ideal para brindar confort y seguridad a los más pequeños. Confeccionada cuidadosamente a mano con materiales suaves y hipoalergénicos, esta manta es perfecta para acompañar cada momento especial, desde la hora de dormir hasta los primeros juegos y descubrimientos. Su diseño encantador y su textura suave estimulan el sentido del tacto, brindando una sensación de calma y familiaridad. Un accesorio imprescindible y un regalo perfecto que no solo acompaña, sino que también envuelve en calidez y ternura.",
-    imageUrl: "https://github.com/invincible-afk/Atenea-Creaciones/blob/main/src/Assets/images/Manta-apego.jpg",
+    imageUrl: "/Assets/images/Manta-apego.jpg",
     category: "Accesorios"
   }
 ];
